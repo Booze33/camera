df179c5c6ec23fa83134ad692633c257c47215cd
ttisloh <ttisloh@gmail.com>
2025-07-25 11:21:13 +0000

    test

---
+++ build/client/assets/Navbar-UBYjyF0E.js
@@ -0,0 +1,1 @@
+import{p as s,v as a}from"./chunk-EF7DTUVF-CCwWxb07.js";const e=()=>s.jsxs("nav",{className:"navbar",children:[s.jsx(a,{to:"/",children:s.jsx("p",{className:"text-2xl font-bold text-gradient",children:"Resumind"})}),s.jsx(a,{to:"/upload",className:"primary-button w-fit",children:"Upload Resume"})]});export{e as N};
